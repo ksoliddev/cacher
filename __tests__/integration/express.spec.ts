94e701b5977b14639cf9f8885a1629867e972730
Kissema Eduardo Rafael <kissol_cfti@hotmail.com>
2020-11-24 14:33:00 +0100

    release cacher-pro

--- __tests__/integration/express.spec.ts
+++ __tests__/integration/express.spec.ts
@@ -3,7 +3,7 @@ const cacher = require('../../')
 describe('Framework Validations', ()=>{
     it('should get the result by cache when registed', async ()=>{
 
-        const request = ;
-        expect(data._cacher.cached).toBe(true);
+        // const request = ;
+        // expect(data._cacher.cached).toBe(true);
     });
 })
--- express/server.ts
+++ express/server.ts
@@ -8,16 +8,39 @@ const cacherMidlleWare = cacher({
     redis : redis.createClient()
 });
 
+var fruits = ['laranja', 'banana', 'tamara'];
 app.get('/express', cacherMidlleWare.handle, (req : any, res : any)=>{
-    res.json({'resultado' : ['laranja', 'banana', 'tamara']});
+    res.json({'resultado' : fruits});
 });
 
 app.get('/express/:id', cacherMidlleWare.handle, (req : any, res : any)=>{
-    res.json(true);
+    res.json({'resultado' : fruits[req.params.id]});
 });
 
 app.post('/express', cacherMidlleWare.handle, (req : any, res : any)=>{
-    res.json(true);
+    if(req.body.fruit){
+        fruits.push(req.body.fruit);
+        res.json("INSERIDO COM SUCESSO!");
+    }
+});
+
+app.put('/express', cacherMidlleWare.handle, (req : any, res : any)=>{
+    if(req.body.fruit){
+        fruits.push(req.body.fruit);
+    }
+    res.json("INSERIDO COM SUCESSO!");
+});
+
+app.patch('/express', cacherMidlleWare.handle, (req : any, res : any)=>{
+    if(req.body.fruit){
+        fruits.push(req.body.fruit);
+    }
+    res.json("INSERIDO COM SUCESSO!");
+});
+
+app.delete('/express/:id', cacherMidlleWare.handle, (req : any, res : any)=>{
+    fruits = fruits.filter((fr : any, idx : number) => idx !== parseInt(req.params.id) );
+    res.json("ELIMINADO COM SUCESSO!");
 });
 
 app.listen(3333, ()=>console.log("Express: Running Server"));
